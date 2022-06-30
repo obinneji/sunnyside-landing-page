34d3deb1e0a074b531c4db46e17676858a9baa89
obinneji <obinnejic@gmail.com>
2022-06-30 05:19:42 -0700

    mobile view initial styles

---
+++ main.js
@@ -0,0 +1,6 @@
+const toggler = document.querySelector('#toggle')
+const mobileNav = document.querySelector('.nav-items')
+toggler.addEventListener("click", function(){
+    mobileNav.style.display = 'block'
+})
+
